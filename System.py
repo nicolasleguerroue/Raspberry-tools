55d15514af915d4fdde966961bdbcc9a088cbf21
nicolasleguerroue <nicolasleguerroue@gmail.com>
2022-05-15 21:57:49 +0200

    Add disk()

--- System.py
+++ System.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 #-- coding: utf-8 --
 
@@ -12,48 +13,47 @@ from blinkt import set_pixel, set_brightness, show, clear
 class System:
 
 	def __init__(self):
-		
+
 		print("#################################")
 		print("########### System Tools ########")
 		print("#################################")
 
 		GPIO.setmode(GPIO.BCM)  	#Standard GPIO Number
 		GPIO.setwarnings(False)
-		
-		set_brightness(0.1)
+
+		set_brightness(0.05)
 
 		self.__TEMPERATURE_LED = 0
 		self.__CPU_LED = 1
 		self.__MEMORY_LED = 2
 		self.__WEB_SERVER_LED = 3
 		self.__INTERNET_LED = 4
-
+		self.__STORAGE_LED = 5
+		self.__USERS_LED = 6
+		
+		self.__DEFAULT_BRIGHTNESS = 0.05
 		self.__BLINKS = 5		#Number of blinks in init step
 		self.__DELAY_BLINK = 0.05 	#s
 
 		self.__localIP = None
-		
+
 		#Display settings
 		cpus = psutil.cpu_count(logical=True)
 		print("CPUs : "+str(cpus))
-		
+
 		cpuFreq = psutil.cpu_freq()
 		print("Frequence : "+str(cpuFreq.current)+" MHz")
 		print("Frequence min : "+str(cpuFreq.min)+" MHz")
 		print("Frequence max : "+str(cpuFreq.max)+" MHz")
 		print("Boot time : "+str(psutil.boot_time())+" s")
-		users = psutil.users()
-		
-		print("Users : ")
-		for u in users:
-			print(">>> name : "+u.name)	
-		print(self.temperatures())
+
+		set_brightness(self.__DEFAULT_BRIGHTNESS)
 
 	def temperatures(self):
 
 		"""Temperatures handler"""
-		self.__lowTemp = 40.0
-		self.__highTemp = 43.0
+		self.__lowTemp = 55.0
+		self.__highTemp = 70.0
 
 		currentTemperature = float(psutil.sensors_temperatures()['cpu_thermal'][0][1])  			
 		#print(shwtemp())
@@ -77,10 +77,31 @@ class System:
 				set_pixel(self.__WEB_SERVER_LED, 0, 255, 0)
 			else:
 				set_pixel(self.__WEB_SERVER_LED, 255, 0, 0)
-		else:	
+		else:
 			set_pixel(self.__WEB_SERVER_LED, 255, 0, 0)
 		return response
 
+	def disk(self):
+		
+		"""Disk handler"""
+		
+		self.__lowStorage = 40.0
+		self.__highStorage = 90.0
+
+		disk = psutil.disk_usage('/')
+
+		total = round(disk.total/1024.0/1024.0,1)
+		percent = disk.percent
+
+		if(percent < self.__lowStorage):
+			set_pixel(self.__STORAGE_LED, 0, 255, 0)
+		elif(percent <= self.__highStorage and percent > self.__lowStorage):
+			set_pixel(self.__STORAGE_LED, 255, 127, 0)
+		else:
+			set_pixel(self.__STORAGE_LED, 255, 0, 0)
+		
+		return percent
+	
 	def network(self):
 		"""Network handler"""
 		self.__key_network = 2 #key of dict when network is used
@@ -108,27 +129,51 @@ class System:
 		show()
 		return self.__interfaces
 
+	def users(self):
+
+		"""Users handler"""
+		users = psutil.users()
+
+		for u in range(0, len(users)):
+			if(len(users) == 1):
+				set_pixel(self.__USERS_LED, 0, 255, 0)
+			else:
+				set_pixel(self.__USERS_LED, 255, 127, 0)
+			show()
+			time.sleep(0.05)
+			set_pixel(self.__USERS_LED, 0, 0,0)
+			show()
+			time.sleep(0.2)
+		
 	def blinkAll(self):
 
 		"""All leds blinks"""
 		clear()
+		set_brightness(1)
 
 		for step in range(0,self.__BLINKS):
-			
+
 			set_pixel(self.__TEMPERATURE_LED,0, 255, 0)
 			set_pixel(self.__CPU_LED,0, 255, 0) 
 			set_pixel(self.__WEB_SERVER_LED,0, 255, 0) 
 			set_pixel(self.__INTERNET_LED,0, 255, 0) 
 			set_pixel(self.__MEMORY_LED,0, 255, 0) 
+			set_pixel(self.__USERS_LED,0, 255, 0) 
+			set_pixel(self.__STORAGE_LED,0, 255, 0) 
+
 			show()
 			time.sleep(self.__DELAY_BLINK)
 			set_pixel(self.__TEMPERATURE_LED,0, 0, 0)
 			set_pixel(self.__CPU_LED,0, 0, 0) 
 			set_pixel(self.__WEB_SERVER_LED,0, 0, 0) 
 			set_pixel(self.__INTERNET_LED,0, 0, 0) 
-			set_pixel(self.__MEMORY_LED,0, 0, 0) 
+			set_pixel(self.__MEMORY_LED,0, 0, 0)
+			set_pixel(self.__USERS_LED,0, 0, 0) 
+			set_pixel(self.__STORAGE_LED, 0, 0, 0)
 			show()
 			time.sleep(self.__DELAY_BLINK)
+
+		set_brightness(self.__DEFAULT_BRIGHTNESS)
 
 	def cpu(self):
 
@@ -145,18 +190,18 @@ class System:
 		else:
 			set_pixel(self.__CPU_LED, 255, 0, 0)
 		show()
-		
+
 		return cpu
 
 	def memory(self):
 
 		"""Check memory usage"""
-		
+
 		self.__lowMemory = 20.0
 		self.__highMemory = 70.0
 
-		raw_memory = psutil.virtual_memory()		
-	
+		raw_memory = psutil.virtual_memory()
+
 		available = round(raw_memory.available/1024.0/1024.0,1)
 		used = round(raw_memory.used/1024.0/1024.0,1)
 		all = round(raw_memory.total/1024.0/1024.0,1)
@@ -170,7 +215,7 @@ class System:
 			set_pixel(self.__MEMORY_LED, 255, 127, 0)
 		else:
 			set_pixel(self.__MEMORY_LED, 255, 0, 0)
-		
+
 		show()
 		return percent
 		#print(raw_memory+" "+available+" "+all) 
@@ -185,11 +230,13 @@ def main():
 
 	while(1):
 	
-		print(sys.temperatures())
-		print(sys.cpu())
-		print(sys.memory())
-		print(sys.network())
-		print(sys.webServer())
+		sys.temperatures()
+		sys.cpu()
+		sys.memory()
+		sys.network()
+		sys.webServer()
+		sys.disk()
+		sys.users()
 		time.sleep(1)
 
 if(__name__ == "__main__"):
---
+++ install.sh
@@ -0,0 +1,29 @@
+# Update
+sudo apt-get update 
+sudo apt-get -y upgrade
+
+
+# Install
+
+## Web server
+sudo apt-get install -y php
+sudo apt-get install -y mariadb-server
+sudo apt-get install -y apache2
+sudo apt-get install -y php-mysql
+
+sudo apt-get install python3-pip
+python3 -m pip install psutil
+python3 -m pip install netifaces
+sudo apt-get install -y git
+
+curl https://get.pimoroni.com/blinkt | bash
+
+sudo apt-get install -y snap
+sudo snap install discord
+sudo snap install code
+
+
+
+
+
+
